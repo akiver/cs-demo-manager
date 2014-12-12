9519286d7a5c2bd504f31c664595f1463bd2ee8c
main() <main@ehvag.de>
2014-12-12 14:40:47 +0100

    Added the DevNullPlayer
    for profiling

---
+++ DevNullPlayer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using DemoInfo;
+
+namespace DevNullPlayer
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			using (var input = File.OpenRead(args[0]))
+				new DemoParser(input).ParseDemo(true);
+		}
+	}
+}
