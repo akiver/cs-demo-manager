65da134e42f1ff6235a6f92d1411e7ffe5963c8c
main() <main@ehvag.eu.org>
2015-02-01 23:06:37 +0100

    Brofiler Verification now errors on any output
    Previously, verification would always be reported as success when 0 was returned, even if output was produced (e.g. assertion failures).
    
    This is take #2, now with new progress reporting architecture: Through the profiling pipe

--- DevNullPlayer/Program.cs
+++ DevNullPlayer/Program.cs
@@ -10,10 +10,26 @@ namespace DevNullPlayer
 		{
 			using (var input = File.OpenRead(args[0])) {
 				var parser = new DemoParser(input);
-				#if DEBUG
-				parser.TickDone += (sender, e) => Console.Write(".");;
-				#endif
 				parser.ParseHeader ();
+
+				#if DEBUG
+				if (args.Length >= 2) {
+					// progress reporting requested
+					using (var progressWriter = new StreamWriter(args[1]) { AutoFlush = false }) {
+						int lastPercentage = -1;
+						while (parser.ParseNextTick()) {
+							var newProgress = (int)(parser.ParsingProgess * 100);
+							if (newProgress != lastPercentage) {
+								progressWriter.Write(lastPercentage = newProgress);
+								progressWriter.Flush();
+							}
+						}
+					}
+
+					return;
+				}
+				#endif
+
 				parser.ParseToEnd ();
 			}
 		}
--- ci/verify.sh
+++ ci/verify.sh
@@ -1,6 +1,7 @@
 set -eu
 
 demofile="$1"
+output_pipe="$2"
 
 # Bitstream-Debugging, no profiling.
-mono --optimize=all DevNullPlayer/bin/Debug/DevNullPlayer.exe "testdemos/$demofile"
+mono --optimize=all DevNullPlayer/bin/Debug/DevNullPlayer.exe "testdemos/$demofile" "/dev/fd/$output_pipe"
