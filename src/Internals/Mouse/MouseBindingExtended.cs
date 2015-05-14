7e21d3f2090ffcbec6a209f017e895b6e94d77cd
AkiVer <ys.renaud@gmail.com>
2015-05-14 22:25:56 +0200

    added ability to navigate between windows with mouse4 and mouse5 buttons

---
+++ src/Internals/Mouse/MouseBindingExtended.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using System.Windows.Markup;
+using CSGO_Demos_Manager.Converters;
+
+namespace CSGO_Demos_Manager.Internals.Mouse
+{
+	public class MouseBindingExtended : MouseBinding
+	{
+		[ValueSerializer(typeof(MouseGestureValueSerializer)), TypeConverter(typeof(MouseGestureExtendedConverter))]
+		public override InputGesture Gesture
+		{
+			get { return base.Gesture; }
+			set { base.Gesture = value; }
+		}
+	}
+}
