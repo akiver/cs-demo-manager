c82c41a7c23b97aa76e1f445115d37c52a9d9d7f
AkiVer <ys.renaud@gmail.com>
2015-09-11 01:55:56 +0200

    removed dead code

--- src/AppSettings.cs
+++ src/AppSettings.cs
@@ -13,7 +13,6 @@ namespace CSGO_Demos_Manager
 		public static string APP_NAME = "CSGO Demos Manager";
 		public static string APP_WEBSITE = "http://csgo-demos-manager.com";
 		public static Version APP_VERSION = new Version("2.1.0");
-		public static string REGISTRY_KEY_DIRECTORIES = "directories";
 		// Don't forget to put your Steam API key into the file "steam_api_key.txt"
 		public static string STEAM_API_KEY = Properties.Resources.steam_api_key;
 		// when an update needs to clear data cache, set it to true to prompt the user to save his comments
@@ -35,17 +34,6 @@ namespace CSGO_Demos_Manager
 		public static string SteamPath()
 		{
 			return (string)Registry.GetValue(@"HKEY_CURRENT_USER\SOFTWARE\Valve\Steam", "SteamPath", null);
-		}
-
-		/// <summary>
-		/// Check if CSGO is installed, we do not need to know where it's installed to launch the game
-		/// </summary>
-		/// <returns>bool true if CSGO is installed, false if not.</returns>
-		public static bool IsCsgoInstalled()
-		{
-			var isInstalled = Registry.GetValue(@"HKEY_CURRENT_USER\SOFTWARE\Valve\Steam\Apps\730", "Installed", null);
-			if (isInstalled == null || (int)isInstalled == 0) return false;
-			return true;
 		}
 
 		/// <summary>
