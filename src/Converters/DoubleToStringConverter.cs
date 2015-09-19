a1d3cc9c326671ff5e4784afc44ecc06d5e18685
AkiVer <ys.renaud@gmail.com>
2015-09-19 17:47:29 +0200

    fixed error binding cast ADR

---
+++ src/Converters/DoubleToStringConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CSGO_Demos_Manager.Converters
+{
+	public class DoubleToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			double number = value as double? ?? 0;
+			return number.ToString(CultureInfo.InvariantCulture);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value;
+		}
+	}
+}
