37976121321cd4faae897d10660c67c9f872fc4f
AkiVer <ys.renaud@gmail.com>
2015-09-20 04:35:10 +0200

    log all errors when a crash occured

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,41 +1,23 @@
-﻿using GalaSoft.MvvmLight.Threading;
-using System.Windows;
-#if RELEASE
-using MahApps.Metro.Controls.Dialogs;
-using System.Windows.Threading;
+﻿using System;
+using GalaSoft.MvvmLight.Threading;
 using CSGO_Demos_Manager.Internals;
-using CSGO_Demos_Manager.Services;
-#endif
 
 namespace CSGO_Demos_Manager
 {
 	public partial class App
 	{
-#if RELEASE
-		private readonly DialogService _dialogService;
-#endif
-
 		public App()
 		{
 			DispatcherHelper.Initialize();
-#if RELEASE
-			_dialogService = new DialogService();
-			DispatcherUnhandledException += Application_DispatcherUnhandledException;
-#endif
-			Exit += Application_Exit;
+			AppDomain.CurrentDomain.UnhandledException += HandleAppDomainUnhandleException;
 		}
 
-		private void Application_Exit(object sender, ExitEventArgs e)
+		private static void HandleAppDomainUnhandleException(object sender, UnhandledExceptionEventArgs args)
 		{
+			Exception e = (Exception)args.ExceptionObject;
+			Logger.Instance.Log(e);
+			System.Windows.MessageBox.Show("An unexpected error occured. Please send the 'errors.log' file accessible from settings." + Environment.NewLine
+				+ "Message Error : " + Environment.NewLine + e.Message, "Error");
 		}
-
-#if RELEASE
-		private async void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-		{
-			Logger.Instance.log(e.Exception);
-			await _dialogService.ShowErrorAsync(e.Exception.Message, MessageDialogStyle.Affirmative);
-			e.Handled = true;
-		}
-#endif
 	}
 }
