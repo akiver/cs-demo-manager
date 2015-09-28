a974c84f692f90ea431952760bfa0f6996dedb21
AkiVer <ys.renaud@gmail.com>
2015-09-28 20:26:43 +0200

    adapted backup to new demos ID

--- src/Services/Serialization/DemoBackupConverter.cs
+++ src/Services/Serialization/DemoBackupConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using CSGO_Demos_Manager.Models;
 using Newtonsoft.Json.Linq;
 
@@ -9,6 +10,8 @@ namespace CSGO_Demos_Manager.Services.Serialization
 	/// </summary>
 	public class DemoBackupConverter : JsonCreationConverter<Demo>
 	{
+		private readonly Regex _oldDemoIdPattern = new Regex("^(?<mapName>.*?)(?<identifier>([0-9]*))$");
+
 		/// <summary>
 		/// Get the custom data from jObject and return a new Demo with its ID and custom data
 		/// </summary>
@@ -19,9 +22,18 @@ namespace CSGO_Demos_Manager.Services.Serialization
 		{
 			if (FieldExists("comment", jObject) && FieldExists("id", jObject))
 			{
-				return new Demo()
+				string currentId = jObject["id"].ToString();
+				string newId = currentId;
+				Match matchId = _oldDemoIdPattern.Match(currentId);
+				if (matchId.Success)
 				{
-					Id = jObject["id"].ToString(),
+					// Add a "_" for new id formatting
+					newId = matchId.Groups["mapName"] + "_" + matchId.Groups["identifier"];
+				}
+				
+				return new Demo
+				{
+					Id = newId,
 					Comment = jObject["comment"].ToString(),
 					Status = jObject["status"].ToString()
 				};
