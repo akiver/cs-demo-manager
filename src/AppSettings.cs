e084eb6cfde34268ae63883c919adfd4eb7cc0a7
AkiVer <ys.renaud@gmail.com>
2015-10-05 22:04:08 +0200

    updated to 2.2.1

--- src/AppSettings.cs
+++ src/AppSettings.cs
@@ -12,7 +12,7 @@ namespace CSGO_Demos_Manager
 		public static string AUTHOR = "AkiVer";
 		public static string APP_NAME = "CSGO Demos Manager";
 		public static string APP_WEBSITE = "http://csgo-demos-manager.com";
-		public static Version APP_VERSION = new Version("2.2.0");
+		public static Version APP_VERSION = new Version("2.2.1");
 		// Don't forget to put your Steam API key into the file "steam_api_key.txt"
 		public static string STEAM_API_KEY = Properties.Resources.steam_api_key;
 		// when an update needs to clear data cache, set it to true to prompt the user to save his comments
