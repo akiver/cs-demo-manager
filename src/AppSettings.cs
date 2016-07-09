0997ba4f01a74b6dfa35270b836dd12ddd0879d5
AkiVer <ys.renaud@gmail.com>
2016-07-09 20:43:59 +0200

    chore: bump version to 2.7.0

--- src/AppSettings.cs
+++ src/AppSettings.cs
@@ -12,7 +12,7 @@ namespace CSGO_Demos_Manager
 		public static string AUTHOR = "AkiVer";
 		public static string APP_NAME = "CSGO Demos Manager";
 		public static string APP_WEBSITE = "http://csgo-demos-manager.com";
-		public static Version APP_VERSION = new Version("2.6.2");
+		public static Version APP_VERSION = new Version("2.7.0");
 		// Don't forget to put your Steam API key into the file "steam_api_key.txt"
 		public static string STEAM_API_KEY = Properties.Resources.steam_api_key;
 		private const string MATCH_LIST_FILENAME = "matches.dat";
