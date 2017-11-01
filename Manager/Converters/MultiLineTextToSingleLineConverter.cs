71a5f3c52333f94ec8cf98aad346f14ac3a12f02
AkiVer <ys.renaud@gmail.com>
2017-11-01 20:40:50 +0100

    feat(ui): add ellipsis on comment column and tooltip on hover
    fix #221

---
+++ Manager/Converters/MultiLineTextToSingleLineConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Manager.Converters
+{
+	public class MultiLineTextToSingleLineConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			string text = value as string;
+			if (text == null) return string.Empty;
+
+			return text.Replace(Environment.NewLine, " ");
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value;
+		}
+	}
+}
