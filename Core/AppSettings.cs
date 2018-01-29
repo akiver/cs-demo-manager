561549359aa69e8ba327087eacc5a0ad6b29d0f7
AkiVer <ys.renaud@gmail.com>
2018-01-29 23:50:38 +0100

    chore: bump to 2.13.0

--- Core/AppSettings.cs
+++ Core/AppSettings.cs
@@ -14,7 +14,7 @@ namespace Core
 		public static string AUTHOR = "AkiVer";
 		public static string APP_NAME = "CSGO Demos Manager";
 		public static string APP_WEBSITE = "https://csgo-demos-manager.com";
-		public static Version APP_VERSION = new Version("2.12.1");
+		public static Version APP_VERSION = new Version("2.13.0");
 		private const string MATCH_LIST_FILENAME = "matches.dat";
 		public const string PROCESS_NAME = "CSGODemosManager";
 		public const string CSGO_PROCESS_NAME = "csgo";
--- setup.iss
+++ setup.iss
@@ -1,4 +1,4 @@
-#define AppVer "2.12.1"
+#define AppVer "2.13.0"
 #define AppName "CSGO Demos Manager"
 #define ExeName "CSGODemosManager.exe"
 #define AppWebsite "https://www.csgo-demos-manager.com"
