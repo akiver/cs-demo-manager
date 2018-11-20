a66d535a968d79f976773ef96576a9056bb5ef97
Cory <coryaccounts@outlook.com>
2018-11-19 23:44:35 -0600

    add de_biome

---
+++ Core/Models/Maps/Biome.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Models.Maps
+{
+    public class Biome : Map
+    {
+        public Biome()
+        {
+            Name = "de_biome";
+            PosX = -2129;
+            PosY = 2368;
+            Scale = 5;
+        }
+    }
+}
