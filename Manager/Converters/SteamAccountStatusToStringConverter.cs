9a47e13403771ea60fc618a481b2e30e250ecbcd
AkiVer <ys.renaud@gmail.com>
2020-03-15 15:00:23 -0700

    fix(suspect): inverted status / account visibility column
    ref #424

--- Manager/Converters/SteamAccountStatusToStringConverter.cs
+++ Manager/Converters/SteamAccountStatusToStringConverter.cs
@@ -8,14 +8,23 @@ namespace Manager.Converters
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			if ((int)value == 0) return Properties.Resources.No;
 			int statusCode = (int)value;
 			switch (statusCode)
 			{
+				case 0:
+					return Properties.Resources.Offline;
 				case 1:
-					return Properties.Resources.Private;
+					return Properties.Resources.Online;
+				case 2:
+					return Properties.Resources.Busy;
 				case 3:
-					return Properties.Resources.Public;
+					return Properties.Resources.Away;
+				case 4:
+					return Properties.Resources.Snooze;
+				case 5:
+					return Properties.Resources.LookingToTrade;
+				case 6:
+					return Properties.Resources.LookingToPlay;
 				default:
 					return Properties.Resources.Unknown;
 			}
--- Manager/Converters/SteamAccountVisibilityToStringConverter.cs
+++ Manager/Converters/SteamAccountVisibilityToStringConverter.cs
@@ -8,28 +8,16 @@ namespace Manager.Converters
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			if ((int)value == 0) return Properties.Resources.No;
 			int statusCode = (int)value;
 			switch (statusCode)
 			{
-				case 0:
-					return Properties.Resources.Offline;
 				case 1:
-					return Properties.Resources.Online;
-				case 2:
-					return Properties.Resources.Busy;
+					return Properties.Resources.Private;
 				case 3:
-					return Properties.Resources.Away;
-				case 4:
-					return Properties.Resources.Snooze;
-				case 5:
-					return Properties.Resources.LookingToTrade;
-				case 6:
-					return Properties.Resources.LookingToPlay;
+					return Properties.Resources.Public;
 				default:
 					return Properties.Resources.Unknown;
 			}
-
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
