e6ad0723ea6d92881aa93ffa7034b25322a46b98
AkiVer <ys.renaud@gmail.com>
2023-12-13 23:21:32 +0100

    chore: upgrade chrome version

--- scripts/electron-vendors.mjs
+++ scripts/electron-vendors.mjs
@@ -1,3 +1,3 @@
 // ! These values must be updated when upgrading Electron, they are available from process.versions.
-export const chrome = '116';
+export const chrome = '120';
 export const node = '18';
