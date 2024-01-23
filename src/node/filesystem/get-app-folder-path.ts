b1be695573d3d86228b3de0968ea6ca31c957873
AkiVer <ys.renaud@gmail.com>
2024-01-23 19:47:14 +0100

    feat(settings): follow XDG base directory specification on linux
    ref #655

--- src/node/filesystem/get-app-folder-path.ts
+++ src/node/filesystem/get-app-folder-path.ts
@@ -1,6 +1,18 @@
 import path from 'node:path';
 import { homedir } from 'node:os';
+import { isLinux } from 'csdm/node/os/is-linux';
 
 export function getAppFolderPath() {
-  return path.join(homedir(), IS_DEV ? '.csdm-dev' : '.csdm');
+  const folderName = IS_DEV ? '.csdm-dev' : '.csdm';
+  if (!isLinux) {
+    return path.join(homedir(), folderName);
+  }
+
+  // https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html#variables
+  const xdgConfigHome = process.env.XDG_CONFIG_HOME;
+  if (typeof xdgConfigHome === 'string' && xdgConfigHome !== '') {
+    return path.join(xdgConfigHome, folderName);
+  }
+
+  return path.join(homedir(), '.config', folderName);
 }
