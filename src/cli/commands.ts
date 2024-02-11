9ffe61a0f2e676c5c1d77d2744273e734daeedcd
GiacoBot <64906428+GiacoBot@users.noreply.github.com>
2024-02-11 14:11:16 +0100

    feat(cli): add json command to export matches in json format

--- src/cli/commands.ts
+++ src/cli/commands.ts
@@ -3,11 +3,13 @@ import { DownloadValveCommand } from './commands/download-valve-command';
 import { DownloadFaceitCommand } from './commands/download-faceit-command';
 import { HelpCommand } from './commands/help-command';
 import { XlsxCommand } from './commands/xlsx-command';
+import { JsonCommand } from './commands/json-command';
 
 export const commands = {
   [AnalyzeCommand.Name]: AnalyzeCommand,
   [DownloadFaceitCommand.Name]: DownloadFaceitCommand,
   [DownloadValveCommand.Name]: DownloadValveCommand,
   [HelpCommand.Name]: HelpCommand,
+  [JsonCommand.Name]: JsonCommand,
   [XlsxCommand.Name]: XlsxCommand,
 };
