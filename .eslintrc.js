a6739a7a089f18ca4f05e1bf83fee2c8ae2a5fc8
AkiVer <ys.renaud@gmail.com>
2024-05-13 17:19:46 +0200

    feat(2d): add elapsed timer next to remaining timer
    ref #830

--- .eslintrc.js
+++ .eslintrc.js
@@ -89,6 +89,7 @@ module.exports = {
           '-',
           '>',
           '?',
+          '/',
           'HLAE',
           'FFmpeg',
           'Valve',
--- src/ui/match/viewer-2d/timer.tsx
+++ src/ui/match/viewer-2d/timer.tsx
@@ -3,13 +3,17 @@ import { useViewerContext } from './use-viewer-context';
 import { formatMillisecondsToTimer } from 'csdm/ui/shared/format-milliseconds-to-timer';
 
 export const Timer = memo(() => {
-  const { timeRemaining, bombPlanted, currentFrame } = useViewerContext();
-  const timer = formatMillisecondsToTimer(timeRemaining);
+  const { timeRemaining, bombPlanted, currentFrame, framerate, round } = useViewerContext();
+  const remainingTimer = formatMillisecondsToTimer(timeRemaining);
   const isBombPlanted = bombPlanted !== null && bombPlanted.frame <= currentFrame;
+  const elapsedFrameCount = currentFrame - round.freezetimeEndFrame;
+  const elapsedTimer = formatMillisecondsToTimer((elapsedFrameCount / framerate) * 1000);
 
   return (
-    <div className="absolute top-16 left-1/2 -translate-x-1/2 py-4 px-8 bg-gray-200 rounded">
-      <p className={`text-body-strong ${isBombPlanted ? 'text-red-700' : ''}`}>{timer}</p>
+    <div className="absolute top-16 left-1/2 -translate-x-1/2 py-4 px-8 bg-gray-200 rounded flex gap-4">
+      <p className={`text-body-strong ${isBombPlanted ? 'text-red-700' : ''}`}>{remainingTimer}</p>
+      <p>/</p>
+      <p className="text-body-strong">{elapsedTimer}</p>
     </div>
   );
 });
