55e4621c2220ea3b134dd35bf1229bde388c79f1
AkiVer <ys.renaud@gmail.com>
2024-07-13 09:10:25 +0200

    fix: error when inserting a huge amount of steam accounts in a single query
    ref #890

--- src/node/database/steam-accounts/insert-steam-accounts.ts
+++ src/node/database/steam-accounts/insert-steam-accounts.ts
@@ -2,21 +2,25 @@ import { db } from '../database';
 import type { InsertableSteamAccount } from './steam-account-table';
 
 export async function insertSteamAccounts(accounts: InsertableSteamAccount[]) {
-  await db
-    .insertInto('steam_accounts')
-    .values(accounts)
-    .onConflict((oc) => {
-      return oc.column('steam_id').doUpdateSet({
-        name: (b) => b.ref('excluded.name'),
-        avatar: (b) => b.ref('excluded.avatar'),
-        creation_date: (b) => b.ref('excluded.creation_date'),
-        economy_ban: (b) => b.ref('excluded.economy_ban'),
-        game_ban_count: (b) => b.ref('excluded.game_ban_count'),
-        has_private_profile: (b) => b.ref('excluded.has_private_profile'),
-        is_community_banned: (b) => b.ref('excluded.is_community_banned'),
-        last_ban_date: (b) => b.ref('excluded.last_ban_date'),
-        vac_ban_count: (b) => b.ref('excluded.vac_ban_count'),
-      });
-    })
-    .execute();
+  const batchSize = 1000;
+  for (let i = 0; i < accounts.length; i += batchSize) {
+    const accountsToInsert = accounts.slice(i, i + batchSize);
+    await db
+      .insertInto('steam_accounts')
+      .values(accountsToInsert)
+      .onConflict((oc) => {
+        return oc.column('steam_id').doUpdateSet({
+          name: (b) => b.ref('excluded.name'),
+          avatar: (b) => b.ref('excluded.avatar'),
+          creation_date: (b) => b.ref('excluded.creation_date'),
+          economy_ban: (b) => b.ref('excluded.economy_ban'),
+          game_ban_count: (b) => b.ref('excluded.game_ban_count'),
+          has_private_profile: (b) => b.ref('excluded.has_private_profile'),
+          is_community_banned: (b) => b.ref('excluded.is_community_banned'),
+          last_ban_date: (b) => b.ref('excluded.last_ban_date'),
+          vac_ban_count: (b) => b.ref('excluded.vac_ban_count'),
+        });
+      })
+      .execute();
+  }
 }
