6ba34e825d51310d517b76a69bfb9eb30eda6ded
AkiVer <ys.renaud@gmail.com>
2024-07-13 09:11:42 +0200

    feat(video): highlight delete sequences button

--- src/ui/match/video/sequences/delete-sequences-button.tsx
+++ src/ui/match/video/sequences/delete-sequences-button.tsx
@@ -4,10 +4,12 @@ import { Button, ButtonVariant } from 'csdm/ui/components/buttons/button';
 import { useDispatch } from 'csdm/ui/store/use-dispatch';
 import { useCurrentMatch } from '../../use-current-match';
 import { deleteSequences } from './sequences-actions';
+import { useCurrentMatchSequences } from './use-current-match-sequences';
 
 export function DeleteSequencesButton() {
   const dispatch = useDispatch();
   const match = useCurrentMatch();
+  const sequences = useCurrentMatchSequences();
 
   const onClick = () => {
     dispatch(
@@ -18,7 +20,7 @@ export function DeleteSequencesButton() {
   };
 
   return (
-    <Button onClick={onClick} variant={ButtonVariant.Danger}>
+    <Button onClick={onClick} variant={sequences.length > 0 ? ButtonVariant.Danger : ButtonVariant.Default}>
       <Trans context="Button">Delete sequences</Trans>
     </Button>
   );
