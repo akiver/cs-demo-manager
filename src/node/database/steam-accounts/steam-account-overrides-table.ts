0e71af4325bf4ae60ff4aff8ef5d136c00f806de
AkiVer <ys.renaud@gmail.com>
2024-08-12 19:44:16 +0200

    chore: deadcode

--- src/node/database/steam-accounts/steam-account-overrides-table.ts
+++ src/node/database/steam-accounts/steam-account-overrides-table.ts
@@ -1,8 +1,4 @@
-import type { Selectable } from 'kysely';
-
 export type SteamAccountOverridesTable = {
   steam_id: string;
   name: string;
 };
-
-export type SteamAccountNameTableRow = Selectable<SteamAccountOverridesTable>;
