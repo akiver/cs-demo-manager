a30bc4e65a46a52e4fbd7a810e1e35aa270c309f
AkiVer <ys.renaud@gmail.com>
2024-10-23 08:21:49 +0200

    feat: add logs for #955

--- src/node/database/psql/ensure-psql-is-installed.ts
+++ src/node/database/psql/ensure-psql-is-installed.ts
@@ -5,6 +5,9 @@ export async function ensurePsqlIsInstalled() {
   try {
     await executePsql('--version');
   } catch (error) {
+    logger.log('psql not found');
+    logger.log(error);
+    logger.log(process.env);
     throw new PsqlNotFound();
   }
 }
