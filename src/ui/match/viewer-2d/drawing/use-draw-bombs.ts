b29e945dc88095c3eb3c595fa86cf6677a1feb25
AkiVer <ys.renaud@gmail.com>
2025-04-12 09:58:58 +0200

    feat(2d): draw dropped bomb position
    ref #1100

--- src/ui/match/viewer-2d/drawing/use-draw-bombs.ts
+++ src/ui/match/viewer-2d/drawing/use-draw-bombs.ts
@@ -1,18 +1,59 @@
+import { useRef } from 'react';
 import type { InteractiveCanvas } from '../../../hooks/use-interactive-map-canvas';
 import { useViewerContext } from '../use-viewer-context';
 import { useBombExplosionImage } from './use-bomb-explosion-image';
 import { useBombImage } from './use-bomb-image';
+import type { PlayerPosition } from 'csdm/common/types/player-position';
 
 export function useDrawBombs() {
-  const { currentFrame, bombPlanted, bombDefused, bombExploded } = useViewerContext();
+  const { currentFrame, bombPlanted, bombDefused, bombExploded, playerPositions } = useViewerContext();
   const bombImage = useBombImage();
   const explosionImage = useBombExplosionImage();
+  const lastBombPosition = useRef<PlayerPosition | null>(null);
 
   const drawBombs = (
     context: CanvasRenderingContext2D,
     { zoomedToRadarX, zoomedToRadarY, zoomedSize }: InteractiveCanvas,
   ) => {
     if (bombPlanted === null || bombPlanted.frame > currentFrame) {
+      const playerPositionsWithBomb = playerPositions.filter((position) => {
+        return position.frame === currentFrame && position.hasBomb;
+      });
+      // A player has a bomb, the bomb indicator will be drawn by the drawPlayers function
+      if (playerPositionsWithBomb.length > 0) {
+        lastBombPosition.current = null;
+        return;
+      }
+
+      const drawBomb = (x: number, y: number) => {
+        const zoomedX = zoomedToRadarX(x);
+        const zoomedY = zoomedToRadarY(y);
+        const bombImageSize = zoomedSize(14);
+        context.drawImage(
+          bombImage,
+          zoomedX - bombImageSize / 2,
+          zoomedY - bombImageSize / 2,
+          bombImageSize,
+          bombImageSize,
+        );
+      };
+
+      if (lastBombPosition.current && lastBombPosition.current.frame <= currentFrame) {
+        drawBomb(lastBombPosition.current.x, lastBombPosition.current.y);
+        return;
+      }
+
+      for (let i = currentFrame - 1; i >= 0; i--) {
+        const lastPlayerPositionWithBomb = playerPositions.find((position) => {
+          return position.frame === i && position.hasBomb;
+        });
+        if (lastPlayerPositionWithBomb) {
+          drawBomb(lastPlayerPositionWithBomb.x, lastPlayerPositionWithBomb.y);
+          lastBombPosition.current = lastPlayerPositionWithBomb;
+          return;
+        }
+      }
+
       return;
     }
 
