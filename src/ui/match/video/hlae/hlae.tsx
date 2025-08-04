4fb39e15867b22071995e9d6bfe2ada4fcf09a59
AkiVer <ys.renaud@gmail.com>
2025-08-04 02:39:20 +0200

    feat(video): remove HLAE warning, it now support the last CS2 update

--- src/ui/match/video/hlae/hlae.tsx
+++ src/ui/match/video/hlae/hlae.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Trans } from '@lingui/react/macro';
 import { HlaeInstallButton } from 'csdm/ui/match/video/hlae/hlae-install-button';
 import { HlaeUpdateButton } from 'csdm/ui/match/video/hlae/hlae-update-button';
 import { HlaeBrowseButton } from 'csdm/ui/match/video/hlae/hlae-browse-button';
@@ -7,24 +6,7 @@ import { Software } from 'csdm/ui/match/video/software';
 import { useInstalledHlaeVersion } from 'csdm/ui/match/video/hlae/use-installed-hlae-version';
 import { HlaeConfigFolderPath } from './hlae-config-folder-path';
 import { HlaeParameters } from './hlae-parameters';
-import { ExclamationTriangleIcon } from 'csdm/ui/icons/exclamation-triangle-icon';
 import { useIsHlaeEnabled } from './use-is-hlae-enabled';
-
-function isUnsupportedVersion(version: string | undefined): boolean {
-  if (!version) {
-    return false;
-  }
-
-  const numbers = version.split('.').map(Number);
-  if (numbers.length < 3) {
-    return false;
-  }
-
-  const [major, minor] = numbers;
-
-  // 2.185.3 is the last HLAE version before the 29/07/2025 CS2 update.
-  return major === 2 && minor <= 185;
-}
 
 export function Hlae() {
   const version = useInstalledHlaeVersion();
@@ -45,22 +27,6 @@ export function Hlae() {
         <HlaeUpdateButton />
         <HlaeBrowseButton />
       </Software>
-      {isUnsupportedVersion(version) && (
-        <div className="flex items-center gap-x-8 max-w-[300px]">
-          <ExclamationTriangleIcon className="size-20 text-red-700 shrink-0" />
-          <div>
-            <p className="text-body-strong">
-              <Trans>This HLAE version doesn't support the last CS2 update, the game will crash!</Trans>
-            </p>
-            <p className="text-body-strong">
-              <Trans>
-                If there is a new version available, please update it. Otherwise, use the "Counter-Strike" recording
-                system in the meantime.
-              </Trans>
-            </p>
-          </div>
-        </div>
-      )}
       <HlaeConfigFolderPath />
       <HlaeParameters />
     </div>
