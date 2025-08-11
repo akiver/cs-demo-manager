b1e68fa57557dbf50d63f2d9c5f6b8273a29de75
AkiVer <ys.renaud@gmail.com>
2025-08-11 03:40:02 +0200

    feat(match): add chat team filter
    ref https://github.com/akiver/cs-demo-manager/discussions/1168

--- src/ui/match/chat-messages/chat-messages.tsx
+++ src/ui/match/chat-messages/chat-messages.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
+import { Trans } from '@lingui/react/macro';
 import { ActionBar } from 'csdm/ui/components/action-bar';
 import { ExportChatMessagesButton } from 'csdm/ui/match/chat-messages/export-chat-messages-button';
 import { ChatMessagesList } from './chat-messages-list';
 import { EmptyChatMessages } from './empty-chat-messages';
 import { useCurrentMatch } from '../use-current-match';
 import { TextInputFilter } from 'csdm/ui/components/inputs/text-input-filter';
+import { Select, type SelectOption } from 'csdm/ui/components/inputs/select';
 
 export function ChatMessages() {
   const match = useCurrentMatch();
+  const [teamName, setTeamName] = useState('');
   const [fuzzySearchText, setFuzzySearchText] = useState('');
 
   const { chatMessages, checksum } = match;
@@ -23,11 +26,35 @@ export function ChatMessages() {
     });
   }
 
+  if (teamName !== '') {
+    const teamPlayers = match.players.filter((player) => player.teamName === teamName);
+    visibleChatMessages = visibleChatMessages.filter(({ senderSteamId }) =>
+      teamPlayers.some((player) => player.steamId === senderSteamId),
+    );
+  }
+
+  const options: SelectOption[] = [
+    { value: '', label: <Trans>All teams</Trans> },
+    { value: match.teamA.name, label: match.teamA.name },
+    { value: match.teamB.name, label: match.teamB.name },
+  ];
+
   return (
     <>
       <ActionBar
         left={<ExportChatMessagesButton checksum={checksum} messages={visibleChatMessages} />}
-        right={<TextInputFilter value={fuzzySearchText} onChange={setFuzzySearchText} />}
+        right={
+          <>
+            <Select
+              options={options}
+              value={teamName}
+              onChange={(teamName) => {
+                setTeamName(teamName);
+              }}
+            />
+            <TextInputFilter value={fuzzySearchText} onChange={setFuzzySearchText} />
+          </>
+        }
       />
       <ChatMessagesList chatMessages={visibleChatMessages} />
     </>
