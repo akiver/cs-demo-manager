236cdd7ea35a33dcb5a10bbd4addd04b03bb0e6e
AkiVer <ys.renaud@gmail.com>
2025-08-26 20:21:32 +0200

    ref: add more log in getDemoAudioData
    ref #1184

--- src/preload/get-demo-audio-data.ts
+++ src/preload/get-demo-audio-data.ts
@@ -12,17 +12,28 @@ type DemoAudioData = {
 };
 
 export async function getDemoAudioData(checksum: string, audioFilePath: string): Promise<DemoAudioData | null> {
-  const extension = path.parse(audioFilePath).ext.slice(1).toLowerCase();
-  if (!supportedAudioExtensions.includes(extension)) {
-    return null;
-  }
-  const fileExists = await pathExists(audioFilePath);
-  if (!fileExists) {
-    return null;
-  }
+  try {
+    const extension = path.parse(audioFilePath).ext.slice(1).toLowerCase();
+    if (!supportedAudioExtensions.includes(extension)) {
+      return null;
+    }
+    const fileExists = await pathExists(audioFilePath);
+    if (!fileExists) {
+      return null;
+    }
 
-  const offsetSeconds = getDemoAudioOffset(checksum);
-  const [audio, bytes] = await Promise.all([loadAudio(audioFilePath), readFile(audioFilePath)]);
+    const offsetSeconds = getDemoAudioOffset(checksum);
+    const [audio, bytes] = await Promise.all([loadAudio(audioFilePath), readFile(audioFilePath)]);
 
-  return { audioFilePath, audio, audioBytes: bytes, offsetSeconds };
+    return { audioFilePath, audio, audioBytes: bytes, offsetSeconds };
+  } catch (error) {
+    logger.error('Error loading demo audio data', audioFilePath);
+    logger.error(error);
+    if (error instanceof Error) {
+      logger.error(error.name);
+      logger.error(error.message);
+      logger.error(error.stack);
+    }
+    throw error;
+  }
 }
