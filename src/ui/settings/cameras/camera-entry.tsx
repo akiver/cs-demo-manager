15bc11b037db77809aad30cf8e0f0cd6d67646e1
AkiVer <ys.renaud@gmail.com>
2025-12-16 21:34:32 +0100

    feat(settings): move copy spec_goto camera action into kebab menu

--- src/ui/settings/cameras/camera-entry.tsx
+++ src/ui/settings/cameras/camera-entry.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Trans } from '@lingui/react/macro';
+import { Trans, useLingui } from '@lingui/react/macro';
 import { Button } from 'csdm/ui/components/buttons/button';
 import { DeleteButton } from 'csdm/ui/components/buttons/delete-button';
 import type { Camera } from 'csdm/common/types/camera';
@@ -8,9 +8,8 @@ import { CameraFormProvider, type CameraFormValues } from './form/camera-form-pr
 import { DeleteCameraDialog } from './delete-camera-dialog';
 import { useDialog } from 'csdm/ui/components/dialogs/use-dialog';
 import { useShowToast } from 'csdm/ui/components/toasts/use-show-toast';
-import { CopyIcon } from 'csdm/ui/icons/copy-icon';
-import { Tooltip } from 'csdm/ui/components/tooltip';
 import { EditCameraDialog } from './edit-camera-dialog';
+import { KebabMenu, KebabMenuItem } from 'csdm/ui/components/kebab-menu';
 
 type Props = {
   camera: Camera;
@@ -20,6 +19,7 @@ export function CameraEntry({ camera }: Props) {
   const getCameraImageSrc = useGetCameraImageSrc();
   const { showDialog } = useDialog();
   const showToast = useShowToast();
+  const { t } = useLingui();
 
   return (
     <div className="w-[294px] rounded border border-gray-300">
@@ -33,20 +33,6 @@ export function CameraEntry({ camera }: Props) {
         </div>
         <div className="mt-8 flex flex-col gap-8">
           <div className="flex flex-wrap items-center gap-8">
-            <Tooltip content={<Trans>Copy spec_goto command</Trans>}>
-              <Button
-                onClick={async () => {
-                  const command = `spec_goto ${camera.x} ${camera.y} ${camera.z} ${camera.pitch} ${camera.yaw}`;
-                  await navigator.clipboard.writeText(command);
-                  showToast({
-                    type: 'success',
-                    content: <Trans>spec_goto command copied to clipboard</Trans>,
-                  });
-                }}
-              >
-                <CopyIcon className="size-20" />
-              </Button>
-            </Tooltip>
             <Button
               onClick={async () => {
                 const previewBase64 = await window.csdm.getCameraPreviewBase64(camera.id);
@@ -80,6 +66,22 @@ export function CameraEntry({ camera }: Props) {
                 showDialog(<DeleteCameraDialog cameraId={camera.id} />);
               }}
             />
+            <div className="h-30">
+              <KebabMenu label={t`Camera actions`}>
+                <KebabMenuItem
+                  onClick={async () => {
+                    const command = `spec_goto ${camera.x} ${camera.y} ${camera.z} ${camera.pitch} ${camera.yaw}`;
+                    await navigator.clipboard.writeText(command);
+                    showToast({
+                      type: 'success',
+                      content: <Trans>spec_goto command copied to clipboard</Trans>,
+                    });
+                  }}
+                >
+                  <Trans>Copy spec_goto command</Trans>
+                </KebabMenuItem>
+              </KebabMenu>
+            </div>
           </div>
         </div>
       </div>
