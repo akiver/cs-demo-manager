70e3bacfd7d50f3be89aae78ba8a5aa6d1bbc727
AkiVer <ys.renaud@gmail.com>
2026-06-28 22:53:21 +0200

    ref: filter chat messages by SteamIDs using SQL

--- src/node/database/chat-messages/fetch-chat-messages.ts
+++ src/node/database/chat-messages/fetch-chat-messages.ts
@@ -2,14 +2,19 @@ import type { ChatMessage } from 'csdm/common/types/chat-message';
 import { db } from 'csdm/node/database/database';
 import { chatMessageRowToChatMessage } from './chat-message-row-to-chat-message';
 
-export async function fetchChatMessages(checksum: string): Promise<ChatMessage[]> {
-  const rows = await db
+export async function fetchChatMessages(checksum: string, steamIds?: string[]): Promise<ChatMessage[]> {
+  let query = db
     .selectFrom('chat_messages')
     .selectAll()
     .leftJoin('steam_account_overrides', 'chat_messages.sender_steam_id', 'steam_account_overrides.steam_id')
     .select([db.fn.coalesce('steam_account_overrides.name', 'chat_messages.sender_name').as('sender_name')])
-    .where('match_checksum', '=', checksum)
-    .execute();
+    .where('match_checksum', '=', checksum);
+
+  if (Array.isArray(steamIds) && steamIds.length > 0) {
+    query = query.where('chat_messages.sender_steam_id', 'in', steamIds);
+  }
+
+  const rows = await query.execute();
   const chatMessages: ChatMessage[] = rows.map((row) => {
     return chatMessageRowToChatMessage(row);
   });
