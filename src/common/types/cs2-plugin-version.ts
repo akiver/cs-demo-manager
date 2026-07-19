6bff579e2d264f7de8895a30c8d14a5976f3711a
AkiVer <ys.renaud@gmail.com>
2026-07-19 02:07:19 +0200

    feat: add new plugin version to support old demos playback

--- src/common/types/cs2-plugin-version.ts
+++ src/common/types/cs2-plugin-version.ts
@@ -1,3 +1,4 @@
+// TODO quality Review versions accuracy
 // Numbers are the CS2 network protocol/Steam "PatchVersion" field that the plugin supports up to
 // the CS2 build it is compatible with.
 // So for example, 14030 means the plugin supports CS2 builds up to the 1.40.3.0 version.
@@ -7,7 +8,12 @@ export const CS2PluginVersion = {
   '14094': '14094', // Support between the 28th July 2025 update ("Animation") and the 14th August 2025 update.
   '14103': '14103', // Support between the 14th August 2025 update and the 17th September 2025 update.
   '14112': '14112', // Support between the 17th September 2025 update and the 15th October 2025 update.
-  '14152': '14152', // Support between the 15th October 2025 update and the 8th April 2026 update ("Animgraph 2")
+  // Support between the 15th October 2025 update and the 8th April 2026 update ("Animgraph 2")
+  // https://github.com/SteamTracking/GameTracking-CS2/commit/c15581f2903059b296c3a71766fcf5effd6278bd
+  '14152': '14152',
+  // Support between "Animgraph 2" update (8th April 2026) and the 09/07/2026 update (1.41.6.8).
+  // https://github.com/SteamTracking/GameTracking-CS2/commit/382cfd288910a2d774a6e6d10428a83416e60da2#diff-1590b30cfb9c4e3984a6ebef95e19ebce0ac0fb786fc22c6d6de4443a52a236f
+  '14168': '14168',
   latest: 'latest',
 } as const;
 
--- src/ui/settings/playback/cs2-plugin-select.tsx
+++ src/ui/settings/playback/cs2-plugin-select.tsx
@@ -27,6 +27,7 @@ export function Cs2PluginSelect() {
   const update14104 = formatDate('2025-09-17', dateOptions);
   const update14112 = formatDate('2025-10-07', dateOptions);
   const animgraph2UpdateDate = formatDate('2026-04-21', dateOptions);
+  const update14168 = formatDate('2026-07-09', dateOptions);
 
   const options: SelectOption<PluginVersion>[] = Object.values(PluginVersion).map((version) => ({
     value: version,
@@ -95,6 +96,15 @@ export function Cs2PluginSelect() {
           </div>
         );
       case PluginVersion[14152]:
+        return (
+          <p>
+            <Trans>
+              You selected a version compatible with CS2 from the {animgraph2UpdateDate} update up to the {update14168}{' '}
+              update.
+            </Trans>
+          </p>
+        );
+      case PluginVersion[14168]:
         return (
           <p>
             <Trans>
