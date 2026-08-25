adc774a22060e8c134a49cb1f17d9335dd0c79d9
AkiVer <ys.renaud@gmail.com>
2026-08-25 21:15:50 +0200

    fix(match): prevent crash when the current match in the GUI is re-analyzed through the CLI

--- src/ui/match/match-actions.ts
+++ src/ui/match/match-actions.ts
@@ -1,7 +1,10 @@
 import { createAction } from '@reduxjs/toolkit';
 import type { Match } from 'csdm/common/types/match';
+import type { ErrorCode } from 'csdm/common/error-code';
 
+export const fetchMatchStart = createAction('match/fetchStart');
 export const fetchMatchSuccess = createAction<{ match: Match }>('match/fetchSuccess');
+export const fetchMatchError = createAction<{ errorCode: ErrorCode }>('match/fetchError');
 export const updateMatchDemoLocationSuccess = createAction<{ checksum: string; demoFilePath: string }>(
   'match/demoLocationUpdated',
 );
