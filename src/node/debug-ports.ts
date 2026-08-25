54382dd630a897c2e1e9711c56bf27a332af0338
AkiVer <ys.renaud@gmail.com>
2026-08-25 23:14:06 +0200

    ref(dev): shared debugger ports

---
+++ src/node/debug-ports.ts
@@ -0,0 +1,5 @@
+// Port on which the WebSocket server (daemon) process inspector listens in development.
+export const SERVER_INSPECTOR_PORT = 9229;
+
+// Port on which the Electron renderer process listens in development.
+export const RENDERER_REMOTE_DEBUGGING_PORT = 9222;
